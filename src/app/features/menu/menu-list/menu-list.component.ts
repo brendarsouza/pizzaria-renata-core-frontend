6bd28e3f6eae27f2c4f3d97a00e7ca98b2975ce3
Brenda Renata de Souza <si.brendarenata@gmail.com>
2025-02-14 23:01:51 -0300

    feat: create components and modules

---
+++ src/app/features/menu/menu-list/menu-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-list',
+  imports: [],
+  templateUrl: './menu-list.component.html',
+  styleUrl: './menu-list.component.scss'
+})
+export class MenuListComponent {
+
+}
